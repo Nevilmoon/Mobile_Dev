831e9c1fa5e2aeef128f0fd993a74bf81a4eb95d
Nevilmoon <skv115a@gmail.com>
2026-05-24 13:09:07 +0300

    Обновили класс CommandRunner, создали новый класс HelpCommand и обновили cli.dart для использования обновленного CommandRunner

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,27 +1,8 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1' ;
-void main(List<String> arguments) async {
-  var runner = CommandRunner();
-  await runner.run(arguments);
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org', 
-    '/api/rest_v1/page/summary/$articleTitle');
-
-  final response = await http.get(url);
-
-  if (response.statusCode == 200){
-    return response.body;
-  }
-
-  return 'Error: Failed to fetch article $articleTitle. Status code ${response.statusCode}';
-
-}
-
-
 
